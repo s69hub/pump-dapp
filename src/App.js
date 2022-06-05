973b85ed41fe1b3d2d01343a4c8a49ef20a0017c
Demetra <41930525+s69hub@users.noreply.github.com>
2022-06-06 03:49:46 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu/Menu";
 import Staking from "./components/Staking/Staking";
 import { StateProvider } from "./contexts/StateContext";
-
+import { useMoralis } from "react-moralis";
 
 function App() {
+  const { Moralis } = useMoralis();
+  useEffect(() => {
+    Moralis.enableWeb3();
+  }, []);
   return (
     <StateProvider>
       <Router>
