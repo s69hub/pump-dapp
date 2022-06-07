fd6121d7d0049c8b56b6c250b24283ab8f980880
Demetra <41930525+s69hub@users.noreply.github.com>
2022-06-07 08:54:29 +0430

    Update Wallet.js

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -7,7 +7,10 @@ import dappbrowser from "../../images/dappbrowser.svg";
 import { BrowserView, MobileView } from "react-device-detect";
 import { getEllipsisTxt } from "../../helpers/formatters";
 import Balances from "../Balances/Balances";
-import { ConnectButton, WalletModal } from "web3uikit";
+import {
+  ConnectButton,
+  WalletModal,
+} from "../../../node_modules/web3uikit/dist";
 
 function Wallet() {
   const [buttonText, setButtonText] = useState("Connect Wallet");
