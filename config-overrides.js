fa07758edc719f8a3be6c21af6dfebd43064f695
Demetra <41930525+s69hub@users.noreply.github.com>
2022-06-07 09:10:12 +0430

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ module.exports = function override(config, env) {
   console.log("override");
   let loaders = config.resolve;
   loaders.fallback = {
+    os: require.resolve("os-browserify/browser"),
     fs: false,
     tls: false,
     net: false,
